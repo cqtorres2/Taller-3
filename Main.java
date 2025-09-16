b1180259577f7a95b6eb68b7ede7c4028ac762b0
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-15 21:18:26 -0500

    final

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
- public static void main(String[] args) {
-    Coche coche1 = new Coche("Renault", "Toyota");
-    
+    public static void main(String[] args) {
+        Coche coche1 = new Coche("Renault", "Toyota");
 
-    System.out.println(coche1);
- }
+        System.out.println(coche1);
+    }
 }
